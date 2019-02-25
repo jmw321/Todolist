c0ac77fe703b36af545681fb7c1e08229f5a83ac
jennawills <jenna.wills321@gmail.com>
2019-02-25 17:54:43 -0500

    updated inner card css to include scrolling overflow and height

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -7,10 +7,12 @@ var Form = ((addShow,showAddClick,boardItems,value,handleSubmit,handleChange,sel
    <div className="form">
    <button className="close" onClick={showAddClick} > X </button>
      <form onSubmit={handleSubmit} >
+     Select Card: <br/>
      <select className="boardItemsOptions" value={selectValue} onChange={handleSelect}>
      {boardItems.map(item => <option id={item.id}> {item.name} </option> )}
-    </select>
-       <input type="text" value={value} onChange={handleChange}>
+    </select> <br/>
+     Enter Task: <br/>
+       <input className="textInput" type="text" value={value} onChange={handleChange}>
        </input>
        <input type="submit" value="Submit">
 
