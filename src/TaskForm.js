62135d851517a3795bc11ff87a9209aee410b508
jennawills <jenna.wills321@gmail.com>
2019-02-25 19:47:01 -0500

    added fafa icon for delete, close. Updated css for form button and layout

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -5,16 +5,17 @@ import './App.css';
 var Form = ((addShow,showAddClick,boardItems,value,handleSubmit,handleChange,selectValue,handleSelect) => { return (
    addShow === true &&
    <div className="form">
-   <button className="close" onClick={showAddClick} > X </button>
+   <button className="close" onClick={showAddClick} > <i class="far fa-window-close fa-2x"></i> </button>
      <form onSubmit={handleSubmit} >
      Select Card:
      <select className="boardItemsOptions" value={selectValue} onChange={handleSelect}>
      {boardItems.map(item => <option id={item.id}> {item.name} </option> )}
-    </select> <br/>
-     Enter Task: 
+    </select> <br/> <br/>
+     Enter Task:
        <input className="textInput" type="text" value={value} onChange={handleChange}>
        </input>
-       <input type="submit" value="Submit">
+       <br/> <br/>
+       <input className="formSubmit" type="submit" value="Submit">
 
        </input>
      </form>
